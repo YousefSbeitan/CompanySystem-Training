b1abef8a873f27ae72189c24616d40a5107bc889
YousefSbeitan <sbetany380@gmail.com>
2026-06-21 11:34:40 +0300

    Add User entity to CompanySystem.Data.Entities
    Introduced a new User class with properties for user ID, username, password hash, role, leader, department, phone number, years of experience, salary, and active status. This class will be used to represent users within the system.

---
+++ CompanySystem.Data/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿namespace CompanySystem.Data.Entities;
+
+public class User
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; }
+
+    public string PasswordHash { get; set; }
+
+    public int RoleId { get; set; }
+
+    public int? LeaderId { get; set; }
+
+    public int? DepartmentId { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public int YearsOfExperience { get; set; }
+
+    public decimal Salary { get; set; }
+
+    public bool IsActive { get; set; }
+}
