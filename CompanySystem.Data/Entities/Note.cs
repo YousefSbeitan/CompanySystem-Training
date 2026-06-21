34e925cbdb8d1a991b522b43408c7fa0bce04b40
YousefSbeitan <sbetany380@gmail.com>
2026-06-21 11:47:19 +0300

    Add Note entity class to data entities
    Introduced the Note class in the CompanySystem.Data.Entities namespace with properties for note metadata, including IDs, type, title, content, and creation date. This provides a model for storing and managing notes in the system.

---
+++ CompanySystem.Data/Entities/Note.cs
@@ -0,0 +1,18 @@
+﻿namespace CompanySystem.Data.Entities;
+
+public class Note
+{
+    public int NoteId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int CreatedBy { get; set; }
+
+    public string NoteType { get; set; }
+
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+}
