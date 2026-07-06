e505254235847427137692e87355e7c9e71afc5f
YousefSbeitan <sbetany380@gmail.com>
2026-07-06 14:24:23 +0300

    Add IMainPageSectionService interface for section management
    Introduced IMainPageSectionService in the Business.Interfaces namespace. This interface provides async CRUD operations for main page sections using DTOs for data transfer.

---
+++ CompanySystem.Business/Interfaces/IMainPageSectionService.cs
@@ -0,0 +1,16 @@
+﻿using CompanySystem.Business.DTOs;
+
+namespace CompanySystem.Business.Interfaces;
+
+public interface IMainPageSectionService
+{
+    Task<IEnumerable<MainPageSectionDto>> GetAllAsync();
+
+    Task<MainPageSectionDto?> GetByIdAsync(int sectionId);
+
+    Task<MainPageSectionDto> CreateAsync(CreateMainPageSectionDto dto);
+
+    Task<MainPageSectionDto?> UpdateAsync(EditMainPageSectionDto dto);
+
+    Task<bool> DeleteAsync(int sectionId);
+}
