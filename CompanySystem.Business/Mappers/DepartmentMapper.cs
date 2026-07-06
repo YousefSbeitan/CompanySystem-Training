2acc255205ab5450529806546f1b8048c0d21f0a
YousefSbeitan <sbetany380@gmail.com>
2026-07-06 13:39:01 +0300

    Add DepartmentMapper for entity-DTO conversions
    Introduced DepartmentMapper class to handle conversions between Department entities and their corresponding DTOs, including methods for mapping to DepartmentDto, creating entities from CreateDepartmentDto, and updating entities with EditDepartmentDto.

---
+++ CompanySystem.Business/Mappers/DepartmentMapper.cs
@@ -0,0 +1,40 @@
+﻿using CompanySystem.Business.DTOs;
+using CompanySystem.Data.Entities;
+
+namespace CompanySystem.Business.Mappers;
+
+public static class DepartmentMapper
+{
+    public static DepartmentDto ToDto(Department department)
+    {
+        return new DepartmentDto
+        {
+            DepartmentId = department.DepartmentId,
+            DepartmentName = department.DepartmentName,
+            ManagerId = department.ManagerId,
+            CreatedBy = department.CreatedBy,
+            CreatedDate = department.CreatedDate,
+            UpdatedBy = department.UpdatedBy,
+            UpdatedDate = department.UpdatedDate
+        };
+    }
+
+
+    public static Department ToEntity(CreateDepartmentDto dto)
+    {
+        return new Department
+        {
+            DepartmentName = dto.DepartmentName,
+            ManagerId = dto.ManagerId
+        };
+    }
+
+
+    public static void UpdateEntity(
+        Department department,
+        EditDepartmentDto dto)
+    {
+        department.DepartmentName = dto.DepartmentName;
+        department.ManagerId = dto.ManagerId;
+    }
+}
