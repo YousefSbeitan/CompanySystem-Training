c5e7fa6e83e25c06449f4df72fb26dee8a0312b5
YousefSbeitan <sbetany380@gmail.com>
2026-07-06 14:03:40 +0300

    Add INoteService interface for async note CRUD operations
    Introduced INoteService in CompanySystem.Business.Interfaces to define asynchronous CRUD methods for notes, utilizing NoteDto, CreateNoteDto, and EditNoteDto. This interface standardizes note management operations across the application.

---
+++ CompanySystem.Business/Interfaces/INoteInteface.cs
@@ -0,0 +1,16 @@
+﻿using CompanySystem.Business.DTOs;
+
+namespace CompanySystem.Business.Interfaces;
+
+public interface INoteService
+{
+    Task<IEnumerable<NoteDto>> GetAllAsync();
+
+    Task<NoteDto?> GetByIdAsync(int noteId);
+
+    Task<NoteDto> CreateAsync(CreateNoteDto dto);
+
+    Task<NoteDto?> UpdateAsync(EditNoteDto dto);
+
+    Task<bool> DeleteAsync(int noteId);
+}
