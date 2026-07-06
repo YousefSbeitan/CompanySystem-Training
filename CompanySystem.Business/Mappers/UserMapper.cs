fb90425cd71e6b6e6f9d1cf220aa65d10b40a163
YousefSbeitan <sbetany380@gmail.com>
2026-07-06 13:51:05 +0300

    Add UserMapper for mapping between User entities and DTOs
    Introduced a static UserMapper class in the Business.Mappers namespace. This class includes methods to convert between User entities and their corresponding DTOs (UserDto, CreateUserDto, EditUserDto), streamlining data transformation between layers.

---
+++ CompanySystem.Business/Mappers/UserMapper.cs
@@ -0,0 +1,58 @@
+﻿using CompanySystem.Business.DTOs;
+using CompanySystem.Data.Entities;
+
+namespace CompanySystem.Business.Mappers;
+
+public static class UserMapper
+{
+    public static UserDto ToDto(User user)
+    {
+        return new UserDto
+        {
+            UserId = user.UserId,
+            Username = user.Username,
+            RoleId = user.RoleId,
+            LeaderId = user.LeaderId,
+            DepartmentId = user.DepartmentId,
+            PhoneNumber = user.PhoneNumber,
+            StartDate = user.StartDate,
+            Salary = user.Salary,
+            IsActive = user.IsActive,
+            CreatedBy = user.CreatedBy,
+            CreatedDate = user.CreatedDate,
+            UpdatedBy = user.UpdatedBy,
+            UpdatedDate = user.UpdatedDate
+        };
+    }
+
+
+    public static User ToEntity(CreateUserDto dto)
+    {
+        return new User
+        {
+            Username = dto.Username,
+            PasswordHash = dto.PasswordHash,
+            RoleId = dto.RoleId,
+            DepartmentId = dto.DepartmentId,
+            PhoneNumber = dto.PhoneNumber,
+            StartDate = dto.StartDate,
+            Salary = dto.Salary,
+            IsActive = dto.IsActive
+        };
+    }
+
+
+    public static void UpdateEntity(
+        User user,
+        EditUserDto dto)
+    {
+        user.Username = dto.Username;
+        user.RoleId = dto.RoleId;
+        user.LeaderId = dto.LeaderId;
+        user.DepartmentId = dto.DepartmentId;
+        user.PhoneNumber = dto.PhoneNumber;
+        user.StartDate = dto.StartDate;
+        user.Salary = dto.Salary;
+        user.IsActive = dto.IsActive;
+    }
+}
