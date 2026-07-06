f6c1ca407c4f03a8d75e766f61a18e65c0554a0a
YousefSbeitan <sbetany380@gmail.com>
2026-07-06 13:52:18 +0300

    Add IUserService interface for user CRUD operations
    Introduced the IUserService interface in the Business.Interfaces namespace to define async CRUD methods for user management. Methods include retrieving all users, getting by ID, creating, updating, and deleting users using DTOs.

---
+++ CompanySystem.Business/Interfaces/IUserInterface.cs
@@ -0,0 +1,16 @@
+﻿using CompanySystem.Business.DTOs;
+
+namespace CompanySystem.Business.Interfaces;
+
+public interface IUserService
+{
+    Task<IEnumerable<UserDto>> GetAllAsync();
+
+    Task<UserDto?> GetByIdAsync(string userId);
+
+    Task<UserDto> CreateAsync(CreateUserDto dto);
+
+    Task<UserDto?> UpdateAsync(EditUserDto dto);
+
+    Task<bool> DeleteAsync(string userId);
+}
